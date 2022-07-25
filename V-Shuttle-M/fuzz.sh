c7303fd14e5ecd50385de9cea38816a35469450a
Zheyu Ma <zheyuma97@gmail.com>
2022-07-25 17:13:10 +0800

    scripts: Add the execution permission
    Signed-off-by: Zheyu Ma <zheyuma97@gmail.com>

--- V-Shuttle-M/fuzz.sh
+++ V-Shuttle-M/fuzz.sh
@@ -1,4 +1,4 @@
-sudo AFL_Fuzzing=1 afl-fuzz -t 5000 -i in -o out -m none -f seed_file -- \
-./qemu-5.1.0/x86_64-softmmu/qemu-system-x86_64 \
+sudo AFL_Fuzzing=1 /home/mzy/truman/third_party/v-shuttle/V-Shuttle-M/afl-2.52b/afl-fuzz -t 5000 -i in -o out -m none -f seed_file -- \
+/home/mzy/truman/third_party/v-shuttle/qemu-5.1.0/build/x86_64-softmmu/qemu-system-x86_64 \
 -display none \
 -device pci-ohci,id=ohci -device usb-tablet,bus=ohci.0,port=1,id=usbdev1 
--- V-Shuttle-S/fuzz.sh
+++ V-Shuttle-S/fuzz.sh
@@ -1,4 +1,4 @@
-sudo AFL_Fuzzing=1 afl-fuzz -t 5000 -i in -o out -m none -f seed -- \
-./qemu-5.1.0/x86_64-softmmu/qemu-system-x86_64 \
+sudo AFL_Fuzzing=1 /home/mzy/truman/third_party/v-shuttle/V-Shuttle-S/afl-seedpool/afl-fuzz -t 5000 -i in -o out -m none -f seed -- \
+../../qemu-5.1.0/build_s/x86_64-softmmu/qemu-system-x86_64 \
 -device pci-ohci,id=ohci -device usb-tablet,bus=ohci.0,port=1,id=usbdev1 \
 -display none
